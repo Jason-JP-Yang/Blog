0614deed3d782f40b4a8d9a273aecff6f1f946a6
Jason-JP-Yang <jiepengyang@outlook.com>
2026-08-30 10:16:11 +0000

    Site Updated: 2026-08-30 10:16:11

--- js/plugins/pangu.js
+++ js/plugins/pangu.js
@@ -1,1 +1,1 @@
-function initPanguJS(){pangu.spacingElementByClassName("markdown-body"),pangu.autoSpacingPage()}document.addEventListener("DOMContentLoaded",initPanguJS);try{swup.hooks.on("page:view",initPanguJS)}catch(n){}
+function initPanguJS(){pangu.spacingElementByClassName("markdown-body"),pangu.autoSpacingPage()}document.addEventListener("DOMContentLoaded",initPanguJS);try{swup.hooks.on("page:view",initPanguJS)}catch(n){}window.addEventListener("redefine:content-injected",initPanguJS);
